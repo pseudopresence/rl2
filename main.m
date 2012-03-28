62afb086092ce0a58231704feea3f4b54e29a99d
Chris Swetenham <cswetenham@gmail.com>
2012-03-28 17:38:57 +0100

    Part I

--- main.m
+++ main.m
@@ -3,30 +3,15 @@
 clc;
 clear all;
 close all;
+dbstop if error;
+dbstop if infnan;
 
-%% Design
+%% Part I - MDP
 
-% Firstly, since we are working in Matlab, we will not attempt to abstract
-% over the details of the problem and produce a general algorithm, since
-% the facilities available for doing so are relatively limited. Instead we
-% will specify the problem and its representation and then write algorithms
-% that solve the specific problem. That said, we will abstract a little
-% where it is useful for code reuse between similar code for our
-% algorithms.
+mdp();
 
-% In some places we will make use of the fact that we have a tabular
-% representation of a function, to speed up the algorithm; in others we
-% will allow for a function to be either represented as a Matlab table or
-% as a Matlab function, which will be either called or indexed with the
-% same syntax, e.g. Q(S, A).
+%% Part II - Bayes filter
 
-
-%% Part I - gridworld, dynamic programming
-
-gridworld();
-
-%% Part II - secretary problem, MC, TD (Q-learning)
-
-secretary_MDP_MC();
+%% Part III - QMDP
 
 
---
+++ mdp.m
@@ -0,0 +1,236 @@
+function mdp()
+
+%% World Setup
+
+stream0 = RandStream('mt19937ar','Seed',0);
+RandStream.setGlobalStream(stream0);
+
+Rot90 = [
+     0 +1;
+    -1  0
+];
+
+% Action representation: Idx mapped to dX, dY by this table
+
+Actions = [
+    +1  0; % 1 E >
+     0 -1; % 2 S V
+    -1  0; % 3 W <
+     0 +1; % 4 N ^
+];
+NActions = size(Actions, 1);
+
+MapWidth = 4;
+MapHeight = 3;
+MapSize = [MapWidth MapHeight];
+NStates = prod(MapSize);
+GoalState = 7;
+
+stateFromPos = @(P) (P(1) - 1) + MapWidth * (P(2) - 1) + 1;
+posFromState = @(S) [mod((S - 1),MapWidth) + 1, floor((S - 1)/MapWidth) + 1];
+
+
+%% Walls
+
+% Compute state transition matrix
+
+% [State x Action] -> State
+StateTransitionTable = zeros([NStates, NActions]);
+
+for A = 1:NActions
+    for S = 1:NStates
+        P = posFromState(S);
+        NP = Actions(A,:) + P;
+        NX = NP(1);
+        NY = NP(2);
+        
+        if (NX < 1 || NX > MapWidth || NY < 1 || NY > MapHeight)
+            StateTransitionTable(S, A) = S;
+        else
+            StateTransitionTable(S, A) = stateFromPos(NP);
+        end
+    end
+end
+
+StateTransitionTable(GoalState, :) = repmat(GoalState, [NActions 1]);
+
+%% Visualisation
+
+% Policy representation: [State] -> Action
+% from the goal state
+StartPolicy = floor(rand(NStates, 1) * 4) + 1;
+
+% Arrows for rendering state transitions
+Arrow(1, :, :) = [
+    0.25 0;
+    0.75 0;
+    0.625 -0.125;
+    0.75 0;
+    0.625 +0.125;
+    0.75 0;
+];
+% Rotate for other directions
+for A = 2:4
+    for L = 1:size(Arrow, 2)
+        Arrow(A,L,:) = Rot90^(A-1) * squeeze(Arrow(1,L,:));
+    end
+end
+
+% Arrows for rendering policies
+% TODO make X/Y component the last, the remove the squeeze()
+% TODO remove the transpose
+% TODO generate with rotation matrix
+% 0 G X
+ActionGlyphs(1, 1, :, :) = [-1 +1; -1 +1]';
+ActionGlyphs(2, 1, :, :) = [-1 +1; +1 -1]';
+% 1 E >
+ActionGlyphs(1, 2, :, :) = [-1 +1; -1 +1]';
+ActionGlyphs(2, 2, :, :) = [+1  0; -1  0]';
+% 2 S V
+ActionGlyphs(1, 3, :, :) = [-1  0; +1  0]';
+ActionGlyphs(2, 3, :, :) = [+1 -1; +1 -1]';
+% 3 W < 
+ActionGlyphs(1, 4, :, :) = [+1 -1; +1 -1]';
+ActionGlyphs(2, 4, :, :) = [+1  0; -1  0]';
+% 4 N ^
+ActionGlyphs(1, 5, :, :) = [-1  0; +1  0]';
+ActionGlyphs(2, 5, :, :) = [-1 +1; -1 +1]';
+% Rescale
+ActionGlyphs = 0.25 * ActionGlyphs;
+
+VizMinX = 0.5;
+VizMaxX = MapWidth+0.5;
+VizMinY = 0.5;
+VizMaxY = MapHeight+0.5;
+
+function drawWalls()
+    line([VizMinX VizMaxX], [VizMinY VizMinY], 'Color', 'red');
+    line([VizMinX VizMaxX], [VizMaxY VizMaxY], 'Color', 'red');
+    line([VizMinX VizMinX], [VizMinY VizMaxY], 'Color', 'red');
+    line([VizMaxX VizMaxX], [VizMinY VizMaxY], 'Color', 'red');
+end
+
+function drawGlyph(Glyph, P)
+    % TODO ugh
+    XS = squeeze([Glyph(1:2:end,1), Glyph(2:2:end,1)])';
+    YS = squeeze([Glyph(1:2:end,2), Glyph(2:2:end,2)])';
+    line(P(1) + XS, P(2) + YS, 'Color', 'green');
+end
+
+function drawStateTransitions()
+    [NStates NActions] = size(StateTransitionTable);
+    for A = 1:NActions
+        for S = 1:NStates
+            P = posFromState(S);
+            if (StateTransitionTable(S, A) ~= S)
+                % TODO put action index at end...
+                drawGlyph(squeeze(Arrow(A,:,:,:)), P);
+            end
+        end
+    end
+end
+
+function drawActionImpl(Glyphs, X, Y, A)
+    % TODO change X,Y to P
+    XS = squeeze(Glyphs(1, A+1,:,:));
+    YS = squeeze(Glyphs(2, A+1,:,:));
+    line(X + XS, Y + YS, 'Color', 'blue');
+end
+
+function drawPolicy(Policy)
+    [NStates] = size(Policy);
+    
+    for S = 1:NStates
+        P = posFromState(S);
+        X = P(1);
+        Y = P(2);
+        drawActionImpl(ActionGlyphs, X, Y, Policy(S));
+    end
+end
+
+function vizPolicy(Fig, V, Policy)
+     figure(Fig);
+        imagesc(reshape(V, MapSize)');
+        colormap('gray');
+        drawWalls();
+        drawPolicy(Policy);
+    axis([VizMinX, VizMaxX, VizMinY, VizMaxY], 'xy', 'equal');
+end
+
+figure(1);
+    drawWalls();
+    drawStateTransitions();
+    drawPolicy(StartPolicy);
+axis([VizMinX, VizMaxX, VizMinY, VizMaxY], 'xy', 'equal');
+
+% writeFigureEPS('StartingPolicy.pdf');
+
+reward = @(S, A, S2) -1 * (S ~= GoalState);
+
+%% We implement state transition probabilities PP(a, s, s') as a function
+%% PP(s, a) -> [States, Probs]; which returns a list of possible next
+%% states and their probabilities.
+
+NormalStateTransitions = @(S, A) deal(StateTransitionTable(S, A), 1);
+
+%% Value iteration
+
+function [Policy] = computeGreedyPolicy(V, StateTransitions, Reward, Discount)
+    Policy = zeros([NStates, 1]);
+    for S = 1:NStates
+        Q = zeros([NActions, 1]);
+        for A = 1:NActions;
+            [S2 Pr] = StateTransitions(S, A);
+            for I = 1:size(S2, 2)
+                Q(A) = Q(A) + Pr(I) * (Reward(S, A, S2(I)) + Discount * V(S2(I)));
+            end
+        end
+        [~, A] = max(Q);
+        Policy(S) = A;
+    end
+end
+
+function [NV] = valueIterationStep(V, StateTransitions, Reward, Discount)
+    NV = zeros([NStates, 1]);
+    for S = 1:NStates
+        Q = zeros([NActions, 1]);
+        for A = 1:NActions;
+            [S2 Pr] = StateTransitions(S, A);
+            for I = 1:size(S2, 2)
+                Q(A) = Q(A) + Pr(I) * (Reward(S, A, S2(I)) + Discount * V(S2(I)));
+            end
+        end
+        [V2 Dummy] = max(Q);
+        NV(S) = V2;
+    end
+end
+
+function valueIteration(Fig, Discount, StateTransitions, MaxPolicyIterations)
+    V = zeros([NStates 1]);
+    for PP = 1:MaxPolicyIterations
+        % Evaluate policy
+        NewV = valueIterationStep(V, StateTransitions, reward, Discount);
+
+        % Compute greedy policy
+        Policy = computeGreedyPolicy(NewV, StateTransitions, reward, Discount);
+
+        if (all(V == NewV))
+            break;
+        end
+        V = NewV;
+
+        vizPolicy(Fig, V, Policy);
+
+        % refresh;
+        % pause(0.1);
+    end
+    fprintf('Value Iteration: Iterations before policy convergence: %d\n', PP);
+end
+fprintf('Value iteration\n');
+Discount = 1;
+MaxPolicyIterations = 1000;
+valueIteration(4, Discount, NormalStateTransitions, MaxPolicyIterations);
+% writeFigureEPS('NormalValueIteration.pdf');
+
+
+end
